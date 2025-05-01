5777d844582005327b2013416244b4346580ce7c
christianfilion <christianfilion8@gmail.com>
2025-05-01 02:14:02 -0400

    main.js

--- main.js
+++ main.js
@@ -2,20 +2,32 @@ require('dotenv').config();
 const { withRetry } = require('./utils');
 const { logToSupabase } = require('./supabase');
 
+// ✅ Import all named scraper functions
+const { scrapeWorkday } = require('./modules/workday-multi');
+const { scrapeTaleo } = require('./modules/taleo');
+const { scrapeLever } = require('./modules/lever');
+const { scrapeGreenhouse } = require('./modules/greenhouse');
+const { scrapeSuccessFactors } = require('./modules/successfactors');
+const { scrapeJobvite } = require('./modules/jobvite');
+const { scrapeBrassring } = require('./modules/brassring');
+const { scrapeRecruitee } = require('./modules/recruitee');
+const { scrapeSmartRecruiters } = require('./modules/smartrecruiters');
+const { scrapeICIMS } = require('./modules/icims');
+
 (async () => {
   console.log("\n🚀 Starting Full Jobify Scraping Engine...\n");
 
   try {
-    await withRetry(() => require('./modules/workday-multi')(), 'Workday');
-    await withRetry(() => require('./modules/taleo')(), 'Taleo');
-    await withRetry(() => require('./modules/lever')(), 'Lever');
-    await withRetry(() => require('./modules/greenhouse')(), 'Greenhouse');
-    await withRetry(() => require('./modules/successfactors')(), 'SuccessFactors');
-    await withRetry(() => require('./modules/jobvite')(), 'Jobvite');
-    await withRetry(() => require('./modules/brassring')(), 'Brassring');
-    await withRetry(() => require('./modules/recruitee')(), 'Recruitee');
-    await withRetry(() => require('./modules/smartrecruiters')(), 'SmartRecruiters');
-    await withRetry(() => require('./modules/icims')(), 'iCIMS');
+    await withRetry(scrapeWorkday, 'Workday');
+    await withRetry(scrapeTaleo, 'Taleo');
+    await withRetry(scrapeLever, 'Lever');
+    await withRetry(scrapeGreenhouse, 'Greenhouse');
+    await withRetry(scrapeSuccessFactors, 'SuccessFactors');
+    await withRetry(scrapeJobvite, 'Jobvite');
+    await withRetry(scrapeBrassring, 'Brassring');
+    await withRetry(scrapeRecruitee, 'Recruitee');
+    await withRetry(scrapeSmartRecruiters, 'SmartRecruiters');
+    await withRetry(scrapeICIMS, 'iCIMS');
 
     await logToSupabase("Success", "✅ Full Jobify scraping engine completed.");
     console.log("\n✅ All scraping modules finished successfully.\n");
